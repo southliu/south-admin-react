1753bc8d0e4cd3c86376e5207b05008accc106ef
southliu <1275093225@qq.com>
2026-01-02 14:41:33 +0800

    test: 菜单切换卡顿

--- build/vite/build.ts
+++ build/vite/build.ts
@@ -6,7 +6,6 @@ import { splitJSModules } from '../utils/helper';
  */
 export function buildOptions(): BuildOptions {
   return {
-    target: ['es2020'],
     chunkSizeWarningLimit: 1000, // 大于1000k才警告
     sourcemap: process.env.NODE_ENV !== 'production', // 非生产环境开启
     minify: 'terser',
