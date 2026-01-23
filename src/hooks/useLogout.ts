7480ea816830d86b05784d0a2cfc0686c0abc8b3
South <1275093225@qq.com>
2026-01-23 15:23:49 +0800

    fix: 切换用户权限菜单无变化

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -7,14 +7,19 @@ export const useLogout = () => {
   const [, , removeToken] = useToken();
   const aliveRef = usePublicStore(useShallow((state) => state.aliveRef));
   const { closeAllTab, setActiveKey } = useTabsStore((state) => state);
+  const setPermissions = useUserStore((state) => state.setPermissions);
+  const setMenuList = useMenuStore((state) => state.setMenuList);
   const clearInfo = useUserStore((state) => state.clearInfo);
   const navigate = useNavigate();
   const location = useLocation();
+
   /** 退出登录 */
   const handleLogout = () => {
     clearInfo();
     closeAllTab();
     setActiveKey('');
+    setMenuList([]);
+    setPermissions([]);
     removeToken();
     aliveRef.current?.destroyAll(); // 清除keepalive缓存
     navigate(`/login?redirect=${location.pathname}${location.search}`);
